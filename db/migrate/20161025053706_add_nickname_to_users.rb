ded35d26c99722dd84eb56e7e053a9df8248d7d2
久保田啓悟 <keigo@kubotamuneko-no-MacBook-Air.local>
2016-10-25 18:23:23 +0900

    delete mg file

--- db/migrate/20161025053706_add_nickname_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNicknameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :nick_name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025053706) do
+ActiveRecord::Schema.define(version: 20161025051036) do
 
   create_table "menus", force: :cascade do |t|
     t.datetime "created_at",               null: false
