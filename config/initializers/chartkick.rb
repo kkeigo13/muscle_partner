11e1765e5a916bee5d6b9d233c4e9530f9a6dfe5
久保田啓悟 <keigo@kubotamuneko-no-MacBook-Air.local>
2016-10-26 00:48:50 +0900

    fix css

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,6 +1,10 @@
 Chartkick.options = {
   height: "200px",
   colors: ["lime", "#999"],
-  width: "700px",
-  margin: "0 auto"
+  width: "100%",
+  margin: "0 auto",
+  hAxis: {
+    title: "日付"
+  }
+
 }
